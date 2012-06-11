22b7863b236330e018fa6ec87b5cd9c7128fb43d
Pavel <pavel@pavel-PC.(none)>
2012-06-12 01:59:19 +0400

    statistics fix

--- text-analyzer/src/main/java/com/mycompany/textanalyzer/ChoiceRunner.java
+++ text-analyzer/src/main/java/com/mycompany/textanalyzer/ChoiceRunner.java
@@ -3,21 +3,21 @@ package com.mycompany.textanalyzer;
 /**
  * Главный класс запуска анализаторов. Опции задаются булевыми 
  * полями useCache (использовать кэш) и usePSQL (использовать 
- * PostgreSQL). Запускает анализатор и выводит информацию после 
- * анализа.
+ * PostgreSQL). Вариант программы c PSQL выбирается изменением 
+ * значения поля version от 1 до 4. Запускает анализатор и выводит
+ * информацию после анализа.
  * @author pavel
  */
 public class ChoiceRunner {
     private final static Boolean useCache = true;
-    private final static Boolean usePSQL = true;
+    private final static Boolean usePSQL = false;
     private final static int version = 2; /*enter from 1 to 4*/
     private final static String textFilePath = "texts/20111225";
     private final static String encoding = "UTF-8";
     
     public static void main(String[] args) {
-        Long timeStart = System.currentTimeMillis();
-        AnalyzerInterface ai = null;
         Statistics stats = new Statistics();
+        AnalyzerInterface ai = null;
         if (usePSQL)
             switch (version) {
                 case 1:
@@ -41,32 +41,7 @@ public class ChoiceRunner {
             ai = new com.mycompany.textanalyzer.analyzer
                     .Analyzer(stats);
         ai.analyze(useCache, textFilePath, encoding);
-        Long timeEnd = System.currentTimeMillis();
-        Long timeRead = stats.getTimeDictRead() - timeStart;
-        Long time = timeEnd - timeStart;
-        System.out.println("Время чтения словаря: " 
-                + String.valueOf((timeRead - timeRead % 1000) 
-                / 1000) + "c " + String.valueOf(timeRead % 1000) 
-                + "мс");
-        System.out.println("Общее время: " 
-                + String.valueOf((time - time % 1000) / 1000) + "c " 
-                + String.valueOf(time % 1000) + "мс");
-        System.out.println("Запросов к словарю: " 
-                + String.valueOf(stats.getCountRequest()) 
-                + ". Успешно: " 
-                + String.valueOf(stats.getCountSuccess()));
-        if (useCache) {
-            System.out.println("Cache hit: " 
-                    + String.valueOf(stats.getCountCacheHit() + "."
-                    + " Cache miss: " 
-                    + String.valueOf(stats.getCountCacheMiss())));
-            System.out.println("Запросов в секунду: " 
-                    + String.valueOf((stats.getCountRequest() 
-                    + stats.getCountCacheHit()) * 1000 / (timeEnd 
-                    - stats.getTimeDictRead())));
-        } else
-            System.out.println("Запросов к словарю в секунду: " 
-                    + String.valueOf(stats.getCountRequest() * 1000
-                    / (timeEnd - stats.getTimeDictRead())));
+        stats.end();
+        stats.print(useCache);
     }
 }
--- text-analyzer/src/main/java/com/mycompany/textanalyzer/Statistics.java
+++ text-analyzer/src/main/java/com/mycompany/textanalyzer/Statistics.java
@@ -6,12 +6,17 @@ package com.mycompany.textanalyzer;
  * @author pavel
  */
 public class Statistics {
-    private int countObjDictRequest = 0;
-    private int countObjDictSuccess = 0;
+    private int countObjDictRequest;
+    private int countObjDictSuccess;
     private long timeDictRead;
-    private int countCacheHit = 0;
-    private int countCacheMiss = 0;
+    private int countCacheHit;
+    private int countCacheMiss;
+    private Long timeStart;
+    private Long timeEnd;
     
+    public Statistics() {
+        timeStart = System.currentTimeMillis();
+    }
     public void increaseCountRequest() {
         countObjDictRequest++;
     }
@@ -50,5 +55,38 @@ public class Statistics {
     
     public int getCountCacheMiss() { 
         return countCacheMiss; 
+    }
+    
+    public void end() {
+        timeEnd = System.currentTimeMillis();
+    }
+    
+    public void print(boolean useCache) {
+        Long timeRead = getTimeDictRead() - timeStart;
+        Long time = timeEnd - timeStart;
+        System.out.println("Время чтения словаря: " 
+                + String.valueOf((timeRead - timeRead % 1000) 
+                / 1000) + "c " + String.valueOf(timeRead % 1000) 
+                + "мс");
+        System.out.println("Общее время: " + String.valueOf((time 
+                - time % 1000) / 1000) + "c " + String.valueOf(time
+                % 1000) + "мс");
+        System.out.println("Запросов к словарю: " 
+                + String.valueOf(getCountRequest()) 
+                + ". Успешно: " 
+                + String.valueOf(getCountSuccess()));
+        if (useCache) {
+            System.out.println("Cache hit: " 
+                    + String.valueOf(getCountCacheHit() + "."
+                    + " Cache miss: " 
+                    + String.valueOf(getCountCacheMiss())));
+            System.out.println("Запросов в секунду: " 
+                    + String.valueOf((getCountRequest() 
+                    + getCountCacheHit()) * 1000 / (timeEnd 
+                    - getTimeDictRead())));
+        } else
+            System.out.println("Запросов к словарю в секунду: " 
+                    + String.valueOf(getCountRequest() * 1000
+                    / (timeEnd - getTimeDictRead())));
     }
 }
